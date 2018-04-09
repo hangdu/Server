b2734be353b8b697e936f5f828588b3a12463675
hangdu <2580392278@qq.com>
2018-04-09 18:44:28 -0400

    4/9 version2 Server can send command 1 and 0 by pushing buttons and client can receives them

--- app/src/main/java/com/example/hang/server/MainActivity.java
+++ app/src/main/java/com/example/hang/server/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.hang.server;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.io.BufferedReader;
 import java.io.DataInputStream;
@@ -16,12 +18,15 @@ import java.net.Socket;
 public class MainActivity extends AppCompatActivity {
     ServerSocket server;
     Socket client;
-    DataInputStream in;
-    DataOutputStream out;
+    Worker worker;
+    Button btn_startCollect;
+    Button btn_stop;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_startCollect = (Button) findViewById(R.id.btn_startCollect);
+        btn_stop = (Button) findViewById(R.id.btn_stop);
 
         try {
             server = new ServerSocket(12345);
@@ -36,33 +41,38 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     client = server.accept();
                     System.out.println("New client");
+                    worker = new Worker(client);
                 } catch (IOException e) {
                     System.out.println("Accept failed: 12345");
                     System.exit(-1);
                 }
-
-                try {
-                    in = new DataInputStream(client.getInputStream());
-                    out = new DataOutputStream(client.getOutputStream());
-
-                } catch (IOException e) {
-                    System.out.println("Read failed");
-                    System.exit(-1);
-                }
-
-                while (true) {
-                    try {
-                        String messageFromClient = in.readUTF();
-                        System.out.println("I got message " + messageFromClient);
-                    } catch (IOException e) {
-                        System.out.println("Read failed");
-                        System.exit(-1);
-                    }
-                }
+//                while (true) {
+//                    try {
+//                        String messageFromClient = in.readUTF();
+//                        System.out.println("I got message " + messageFromClient);
+//                    } catch (IOException e) {
+//                        System.out.println("Read failed");
+//                        System.exit(-1);
+//                    }
+//                }
             }
         };
         Thread connectThread = new Thread(runnable);
         connectThread.start();
+
+
+        btn_startCollect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                worker.addCommand(1);
+            }
+        });
+        btn_stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                worker.addCommand(0);
+            }
+        });
     }
 }
 
--- app/src/main/java/com/example/hang/server/Worker.java
+++ app/src/main/java/com/example/hang/server/Worker.java
@@ -1,8 +1,62 @@
 package com.example.hang.server;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by hang on 4/9/18.
  */
 
 public class Worker {
+    DataInputStream in;
+    DataOutputStream out;
+    Socket client;
+    BlockingDeque<Integer> queue;
+    public Worker(Socket client) {
+        this.client = client;
+        queue = new LinkedBlockingDeque<>();
+        try {
+            in = new DataInputStream(client.getInputStream());
+            out = new DataOutputStream(client.getOutputStream());
+
+        } catch (IOException e) {
+            System.out.println("Read failed");
+            System.exit(-1);
+        }
+
+        new Thread(writeTask).start();
+    }
+
+    Runnable writeTask = new Runnable() {
+        @Override
+        public void run() {
+            while (true) {
+                Integer command = null;
+                try {
+                    command = queue.poll(300, TimeUnit.MILLISECONDS);
+                    if (command != null) {
+                        System.out.println("command="+command);
+                        out.writeUTF(String.valueOf(command));
+                        out.flush();
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    };
+
+
+    public void addCommand(int command) {
+        queue.add(command);
+    }
+
 }
